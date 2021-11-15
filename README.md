7cff59c099f4a71e41bbf6ff6db716f2ef23b608
imxianjun <imxianjun@gmail.com>
2021-11-15 19:31:43 +0800

    add merge sorted array

---
+++ README.md
@@ -0,0 +1,5 @@
+# Leetcode Algorithm
+
+## Day
+
+1. 2021-11-15 MergeSortedArray
---
+++ src/com/inbetter/study/algorithm/MergeSortedArray.java
@@ -0,0 +1,25 @@
+package com.inbetter.study.algorithm;
+
+/**
+ * @see <a href="https://leetcode.com/problems/merge-sorted-array/">
+ * 88.merge-sorted-array
+ * </a>
+ */
+public class MergeSortedArray {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] < nums2[j]) {
+                nums1[k--] = nums2[j--];
+            } else {
+                nums1[k--] = nums1[i--];
+            }
+        }
+
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+
+}
